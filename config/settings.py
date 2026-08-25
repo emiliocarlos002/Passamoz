cf0d353359342d675cce1b951195dbe9e2e97da7
emiliocarlos002 <emiliocarlossantana24@gmail.com>
2026-08-25 01:27:57 +0100

    corrigir allowed_hosts

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "dev-only-change-this-key")
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = [ "passamoz.onrender.com", "localhost", "127.0.0.1", ]
+ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = [ "https://passamoz.onrender.com", ]
 
